14507e4142b3ddc39367c6d324a18077d3b92bee
Toby Jee <tmjee@apache.org>
2006-09-04 05:35:34 +0000

    - renamed ${groupId} to ${package} such that -DpackageName argument will take effect - removed resources not required (src/test/resources/com/*) since the package in resource can't be change dynamically, there's no point to have it there.
    git-svn-id: https://svn.apache.org/repos/asf/struts/maven/trunk@439936 13f79535-47bb-0310-9956-ffa450edef68

--- struts2-archetype-starter/src/main/resources/archetype-resources/src/main/java/HelloWorldAction.java
+++ struts2-archetype-starter/src/main/resources/archetype-resources/src/main/java/HelloWorldAction.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ${groupId};
+package ${package};
 
 import com.opensymphony.xwork2.ActionSupport;
 
