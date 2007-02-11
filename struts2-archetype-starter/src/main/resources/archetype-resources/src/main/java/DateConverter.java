1dec10394f625abf01cf3d13faf68e446228f467
Ian Roughley <roughley@apache.org>
2007-02-11 20:31:09 +0000

    Changed converter so that it extended the StrutsTypeConverter (and is used in the example application).  Modified the Index action so that it uses the converter to provide a date compatible with the HelloWorldAction
    git-svn-id: https://svn.apache.org/repos/asf/struts/maven/trunk@506121 13f79535-47bb-0310-9956-ffa450edef68

--- struts2-archetype-starter/src/main/resources/archetype-resources/src/main/java/DateConverter.java
+++ struts2-archetype-starter/src/main/resources/archetype-resources/src/main/java/DateConverter.java
@@ -23,27 +23,27 @@ import java.text.SimpleDateFormat;
 import java.text.ParseException;
 import java.util.Map;
 import java.util.Date;
+import com.opensymphony.xwork2.util.TypeConversionException;
 
 /**
  * 
  */
-public class DateConverter {
+public class DateConverter extends StrutsTypeConverter {
     public Object convertFromString(Map context, String[] values, Class toClass) {
         if (values != null && values.length > 0 && values[0] != null && values[0].length() > 0) {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
             try {
                 return sdf.parse(values[0]);
             }
             catch(ParseException e) {
-                e.printStackTrace();
-                return "";
+                throw new TypeConversionException(e);
             }
         }
         return null;
     }
     public String convertToString(Map context, Object o) {
         if (o instanceof Date) {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
             return sdf.format((Date)o);
         }
         return "";
--- struts2-archetype-starter/src/main/resources/archetype-resources/src/main/java/IndexAction.java
+++ struts2-archetype-starter/src/main/resources/archetype-resources/src/main/java/IndexAction.java
@@ -19,14 +19,18 @@ package ${package};
 
 import com.opensymphony.xwork2.ActionSupport;
 import java.util.Date;
+import com.opensymphony.xwork2.conversion.annotations.Conversion;
+import com.opensymphony.xwork2.conversion.annotations.TypeConversion;
 
 /**
  * 
  */
+@Conversion()
 public class IndexAction extends ActionSupport {
     
     private Date now = new Date(System.currentTimeMillis());
     
+    @TypeConversion(converter = "com.fdar.apress.s2.DateConverter")
     public Date getDateNow() { return now; }
     
     public String execute() throws Exception {
