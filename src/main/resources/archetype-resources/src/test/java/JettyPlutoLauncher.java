d486e8d405bc690cc01ea88aaed437a239ca9e55
Nils-Helge Garli <nilsga@apache.org>
2009-01-18 10:38:09 +0000

    Updated for struts2 2.1.6 and maven-jetty-pluto-embeddded 1.0.1
    git-svn-id: https://svn.apache.org/repos/asf/struts/maven/trunk/struts2-archetype-portlet@735430 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/resources/archetype-resources/src/test/java/JettyPlutoLauncher.java
+++ src/main/resources/archetype-resources/src/test/java/JettyPlutoLauncher.java
@@ -7,7 +7,7 @@ import org.mortbay.jetty.webapp.WebAppContext;
 
 public class JettyPlutoLauncher {
  public static void main(String[] args) throws Exception {
-     System.setProperty("org.apache.pluto.embedded.portletId", "HelloPortlet");
+     System.setProperty("org.apache.pluto.embedded.portletIds", "HelloPortlet");
      Server server = new Server(8080);
      WebAppContext webapp = new WebAppContext("src/main/webapp", "/${artifactId}");
      webapp.setDefaultsDescriptor("/WEB-INF/jetty-pluto-web-default.xml");
